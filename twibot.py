fb24dda98f46556881443efbbd479c27e0fb5f3d
dferng12 <dferng12@estudiantes.unleon.es>
2018-06-17 15:53:24 +0200

    Keys from environment variables

--- twibot.py
+++ twibot.py
@@ -7,8 +7,8 @@ import os
 import ast
 
 def oauth_req(url, http_method, post_body="", http_headers=None):
-    consumer = oauth2.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
-    token = oauth2.Token(key=ACCESS_KEY, secret=ACCESS_SECRET)
+    consumer = oauth2.Consumer(key=os.environ['CONSUMER_KEY'], secret=os.environ['CONSUMER_SECRET'])
+    token = oauth2.Token(key=os.environ['ACCESS_KEY'], secret=os.environ['ACCESS_SECRET'])
     client = oauth2.Client(consumer, token)
     resp, content = client.request(url, method=http_method, headers=None)
     return content
@@ -23,18 +23,16 @@ def fetch_image(media_url):
 def get_real_text(prevText):
 	
 	text = prevText
-	print 'Limpiando: ' + prevText
-	#Eliminando los hipervinculos del texto
+
+	#Deleting media links from tweet
 	while prevText.find('https://t.co') != -1:
 		start_index = prevText.find('https://t.co')
 		prevText = prevText[:start_index]
 
-	#En caso de que se adjunte solo como texto un link a una imagen, para que no crashee
+	#Check if the only text of the message was the media link
 	text = prevText
 	prevText = prevText.replace(' ', '')
 	
-	print len(prevText)
-
 	if len(prevText) == 0:
 		text = messages[current]['text']
 
@@ -51,10 +49,10 @@ def fetch_gifVideo(media_url):
 if __name__ == '__main__':
 	current = 0
 	end = 0
-	api = twitter.Api(consumer_key=CONSUMER_KEY,
-	                  consumer_secret=CONSUMER_SECRET,
-	                  access_token_key=ACCESS_KEY,
-	                  access_token_secret=ACCESS_SECRET)
+	api = twitter.Api(consumer_key=os.environ['CONSUMER_KEY'],
+	                  consumer_secret=os.environ['CONSUMER_SECRET'],
+	                  access_token_key=os.environ['ACCESS_KEY'],
+	                  access_token_secret=os.environ['ACCESS_SECRET'])
 
 	lastTweet = api.GetHomeTimeline(count=1)
 	messages = api.GetDirectMessages(return_json=True)
